60352e5fb0d3dc410c8ad7410bf2db6d1da1fe40
Arvesh Gosine <arveshgosine@gmail.com>
2025-05-18 22:18:48 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './Components/Layout';
+import Home from './Components/Home';
+
+function App() {
+
+  return (
+    <div>
+      <Routes>
+        <Route path = "/" element = {<Layout/>}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
