7c13620e3ce98a63db454d67f827c52face3ff85
Arvesh Gosine <arveshgosine@gmail.com>
2025-05-30 16:24:25 -0400

    added animated background

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,11 +1,39 @@
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    FinisherHeader: any;
+  }
+}
 
 function Home() {
+  // Initialize FinisherHeader after the component mounts
+  useEffect(() => {
+    if (window.FinisherHeader) {
+      new window.FinisherHeader({
+        count: 6,
+        size: { min: 1300, max: 1500, pulse: 0 },
+        speed: { x: { min: 0.8, max: 1 }, y: { min: 0.1, max: 0.6 } },
+        colors: {
+          background: "#260303",
+          particles: ["#2e0308", "#080808", "#170011","#220128" ],
+        },
+        blending: "lighten",
+        opacity: { center: 0.5, edge: 0.05 },
+        skew: -2,
+        shapes: ["c"],
+      });
+    }
+  }, []);
 
   return (
     <div>
-
+      <div
+        className="header finisher-header"
+        style={{ width: "100%", height: "700px" }}
+      ></div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -2,9 +2,16 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../Sidebar";
 import "./index.scss";
 
+/* 
+  -The project has been layed out to be expanded on in the future.
+  -For now it will be one page all contained here in the layout component.
+
+*/
+
 const Layout = () => {
   return (
     <div className="App">
+  
       <Sidebar />
 
       <Outlet />
