a74efab5c21c5f3101ceaae83799b17cc0037057
Arvesh Gosine <arveshgosine@gmail.com>
2026-06-21 17:21:42 -0400

    added: content slider background

--- src/Components/Content Slider/index.tsx
+++ src/Components/Content Slider/index.tsx
@@ -5,7 +5,9 @@ function ContentSlider() {
     <div className="content-container">
       <h1 className="slider-header">Projects</h1>
       <div className="divider"></div>
-      <div className="content-background"></div>
+      <div className="content-background">
+        <div className="slider-background"></div>
+      </div>
     </div>
   );
 }
