a7b54ec9abbeb1bbf1093cbf7e60449268deeb64
Arvesh Gosine <arveshgosine@gmail.com>
2026-07-02 00:13:10 -0400

    added: project data with typing

---
+++ src/types/projects.ts
@@ -0,0 +1,10 @@
+export type CardType = "mobile" | "desktop";
+
+export interface Project {
+  id: string;
+  title: string;
+  cardType: CardType;
+  bullets: string[];
+  tags: string[];
+  images: string[];
+}
