c8707057e69897738e8250306e2dace1b2b2d1fb
Arvesh Gosine <arveshgosine@gmail.com>
2026-08-07 22:36:37 -0400

    added: base typography for about section card

--- src/Components/About Section/index.tsx
+++ src/Components/About Section/index.tsx
@@ -4,21 +4,27 @@ function AboutSection() {
   return (
     <div className="about-container">
       <div className="background-shapes"></div>
-      <div className="bio-section">
-        <h2 className="bio-header">BIO</h2>
-        <p className="bio-paragraph">
-          Full Stack Developer integrating AI/ML solutions into web, desktop and
-          mobile applications. Also proficient in Data Analytics.
-        </p>
-        <h2 className="bio-header">EDUCATION</h2>
-        <div className="education-section">
-          <div className="education-details">
-            <p>BSc. Computer Science (Special)</p>
-            <p>University of The West Indies, St. Augustine</p>
-          </div>
-          <div className="gpa">GPA: 3.86/4.0</div>
+      <div className="about-card">
+        <div className="section">
+          <h2 className="about-header">BIO</h2>
+          <p className="about-paragraph">
+            Full Stack Developer integrating AI/ML solutions into web, desktop
+            and mobile applications. Also proficient in Data Analytics.
+          </p>
         </div>
-        <h2 className="bio-header">
+        <div className="section">
+          <h2 className="about-header">EDUCATION</h2>
+          <div className="education-section">
+            <div className="education-details">
+              <p className="about-paragraph">BSc. Computer Science (Special)</p>
+              <p className="about-paragraph">
+                University of The West Indies, St. Augustine
+              </p>
+            </div>
+            <div className="gpa">GPA: 3.86/4.0</div>
+          </div>
+        </div>
+        <h2 className="about-header">
           EMAIL: <span>arveshgosine04@gmail.com</span>
         </h2>
         <a className="resume-download">Resume</a>
